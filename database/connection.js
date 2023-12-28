4aea20550359943479667efb40d81f32809de321
mdavidrodriguez <mdavidrodriguez@unicesar.edu.co>
2023-12-28 13:48:02 -0500

    api rest red social

---
+++ database/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connection = async () => {
+
+    try {
+        await mongoose.connect("mongodb://127.0.0.1:27017/red_social");
+        console.log("Conectado correctamente a bd: mi_redsocial");
+    } catch (error) {
+        console.log(error);
+        throw new Error("No se ha podido conectar a la base de datos !!");
+    }
+}
+
+module.exports = connection;
